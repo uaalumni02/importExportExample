8867bf2009e81e5d8a148df67f457558d4283589
Demeco <dbell1911@gmail.com>
2018-08-01 21:11:14 -0400

    more practice

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 import salutation from "./functions/greeting";
+import { purchasePrice as payup, amountLeftOver as luckyyou } from "./functions/purchase";
 
 console.log(salutation(1400));
+console.log(payup(76, 900))
+console.log(luckyyou(150, 50))
---
+++ functions/purchase.js
@@ -0,0 +1,15 @@
+const purchasePrice = (item1, item2) =>
+    (item1 + item2)
+
+const amountLeftOver = (item1, item2) =>
+    (item1 - item2)
+
+    export {
+        purchasePrice,
+        amountLeftOver
+    }
+    
+
+
+
+
